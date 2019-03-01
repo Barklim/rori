1f79b37dc2f9c4a32b2a1a0cee27a1a03b66d4de
Barklim <kliment.barkalov@gmail.com>
2019-03-02 01:33:56 +0300

    Updates

---
+++ precache-manifest.8485605925c04f16b04ba18f5aa0130c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c4cad50d083c06ecbc8",
+    "url": "/rori/static/js/runtime~main.5c4cad50.js"
+  },
+  {
+    "revision": "311ac4418bf33f6fd266",
+    "url": "/rori/static/js/main.311ac441.chunk.js"
+  },
+  {
+    "revision": "6289b6dd64317b914ac1",
+    "url": "/rori/static/js/1.6289b6dd.chunk.js"
+  },
+  {
+    "revision": "311ac4418bf33f6fd266",
+    "url": "/rori/static/css/main.c1587e36.chunk.css"
+  },
+  {
+    "revision": "716d7b153a38893ae78ad0634134aa70",
+    "url": "/rori/index.html"
+  }
+];
